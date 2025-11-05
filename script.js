dce8c37014cfc0a773e9c7241a9085d9a9f6ede4
Carl Barcenas <carlanthony.barcenas@marquette.edu>
2025-11-04 21:52:45 -0600

    finished product of absurd pun

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const button = document.querySelector('button');
-const body = document.querySelector('');
+const body = document.querySelector('.body-container');
 
 button.addEventListener("click", () => {
     button.setAttribute("id", "active");
 
     const message = document.createElement("h1");
-    message.textContent = "You are sub-lime!"
-    document.appendChild(message);
+    message.textContent = "You are"
+    button.before(message);
+
+    const sublimeContainer = document.createElement("div");
+    sublimeContainer.setAttribute("class", "sublime-container");
+
+    const message2a = document.createElement("h1");
+    const message2b = document.createElement("h1");
+    message2a.textContent = "sub";
+    message2b.textContent = "lime!";
+
+    message2a.style.color = 'rgba(35, 219, 41, 1)';
+    message2b.style.color = 'rgba(253, 187, 45, 1)';
+
+    message.setAttribute('class', 'part1');
+    message2a.setAttribute('class', 'part2');
+    message2b.setAttribute('class', 'part2');
+
+    sublimeContainer.appendChild(message2a);
+    sublimeContainer.appendChild(message2b);
+    button.after(sublimeContainer);
 })
